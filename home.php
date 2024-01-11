2cc2a273ee3888d21d095679dca2f46bf3b9d3f7
Sam Lo <sam_noreply@gmail.com>
2024-01-11 23:19:39 +0800

    initial commit

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo List</title>
+</head>
+
+<body>
+
+    <form action="home.php" method="POST">
+        <input type="text" name="todo" id="todo" placeholder="Enter a new task">
+        <button>Create</button>
+    </form>
+    <ul>
+        <li>wash</li>
+        <li>study</li>
+    </ul>
+
+    <script src="app.js"></script>
+</body>
+
+</html>
