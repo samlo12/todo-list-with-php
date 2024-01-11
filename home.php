a69388aa777ba3e56bc661eb2bc4d6375228568a
Sam Lo <sam_noreply@gmail.com>
2024-01-12 00:31:48 +0800

    add create task function

--- home.php
+++ home.php
@@ -9,15 +9,43 @@
 
 <body>
 
+    <?php
+    require_once "database.php";
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['create'])) {
+        // collect value of input field
+        $task = $_POST['todo'];
+        if (!empty($task)) {
+            $sql = "INSERT INTO todo (task, completed, user_id)
+VALUES ('$task', false, '1')";
+            if (!$conn->query($sql)) {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        }
+    }
+    ?>
+
+    <?php
+    $sql = "SELECT task, completed FROM todo";
+    $result = $conn->query($sql);
+    ?>
+
     <form action="home.php" method="POST">
         <input type="text" name="todo" id="todo" placeholder="Enter a new task">
-        <button>Create</button>
+        <input type="submit" name="create" value="Create">
     </form>
     <ul>
-        <li>wash</li>
-        <li>study</li>
+        <?php
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                echo "<li>{$row['task']}</li>";
+            }
+        }
+        ?>
     </ul>
-
+    <?php
+    $conn->close();
+    ?>
     <script src="app.js"></script>
 </body>
 
