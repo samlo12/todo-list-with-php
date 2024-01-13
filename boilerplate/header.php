85a30173d2134405162f56a7e393c08c3144b3f8
Sam Lo <sam_noreply@gmail.com>
2024-01-14 01:47:49 +0800

    add some style

--- boilerplate/header.php
+++ boilerplate/header.php
@@ -10,3 +10,22 @@
 </head>
 
 <body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary mb-3" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Todo List</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/todo-list/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/todo-list/register">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
--- home.php
+++ home.php
@@ -1,55 +1,55 @@
 <?php require_once "./boilerplate/header.php" ?>
-
-<?php
-require_once "database.php";
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['create'])) {
-    // collect value of input field
-    $task = $_POST['todo'];
-    if (!empty($task)) {
-        $sql = "INSERT INTO todo (task, completed, user_id)
+<div>
+    <?php
+    require_once "database.php";
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['create'])) {
+        // collect value of input field
+        $task = $_POST['todo'];
+        if (!empty($task)) {
+            $sql = "INSERT INTO todo (task, completed, user_id)
 VALUES ('$task', false, '1')";
-        if (!$conn->query($sql)) {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            if (!$conn->query($sql)) {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
         }
     }
-}
-?>
-
-<?php
-$sql = "SELECT id, task, completed FROM todo";
-$result = $conn->query($sql);
-?>
-
-<form action="home.php" method="POST">
-    <input type="text" name="todo" id="todo" placeholder="Enter a new task">
-    <input type="submit" name="create" value="Create">
-</form>
-<ul>
-    <?php
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while ($row = $result->fetch_assoc()) {
-            $id = $row['id'];
-            echo "<li class='task'>";
-            echo "<span class='text'>{$row['task']}</span>";
-    ?>
-            <button class="editBtn">Edit</button>
-
-            <form class="hide" action="edit.php" method="POST">
-                <input type="hidden" name="id" value="<?php echo $id ?>">
-                <input type="text" name="todo" id="todo" value="<?php echo $row['task'] ?>" placeholder="Task">
-                <input type="submit" name="editContent" value="Edit">
-            </form>
-    <?php
-            echo "<a href='delete.php?id=$id'>Delete</a>";
-            echo "</li>";
-        }
-    };
     ?>
 
-</ul>
-<?php
-$conn->close();
-?>
+    <?php
+    $sql = "SELECT id, task, completed FROM todo";
+    $result = $conn->query($sql);
+    ?>
 
+    <form action="home.php" method="POST">
+        <input type="text" name="todo" id="todo" placeholder="Enter a new task">
+        <input type="submit" name="create" value="Create">
+    </form>
+    <ul>
+        <?php
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                $id = $row['id'];
+                echo "<li class='task'>";
+                echo "<span class='text'>{$row['task']}</span>";
+        ?>
+                <button class="editBtn">Edit</button>
+
+                <form class="hide" action="edit.php" method="POST">
+                    <input type="hidden" name="id" value="<?php echo $id ?>">
+                    <input type="text" name="todo" id="todo" value="<?php echo $row['task'] ?>" placeholder="Task">
+                    <input type="submit" name="editContent" value="Edit">
+                </form>
+        <?php
+                echo "<a href='delete.php?id=$id'>Delete</a>";
+                echo "</li>";
+            }
+        };
+        ?>
+
+    </ul>
+    <?php
+    $conn->close();
+    ?>
+</div>
 <?php require_once "./boilerplate/footer.php" ?>
